1ade2775e175de7934cbe546b84f0b91e584a8cd
ibeneme <ikennaibenemee@gmail.com>
2026-03-19 19:09:08 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+
+
 import React from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 
--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux"; // Import Provider
 import "./index.css";
 import { store } from "./api/store";
+import './assets/fonts/fonts.css'
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
