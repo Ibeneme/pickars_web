53348034ac62f96e1f4e7afb510b0adbede0b9e2
ibeneme <ikennaibenemee@gmail.com>
2026-09-18 19:17:39 +0100

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,17 @@
-theme: {
-    extend: {
-      fontFamily: {
-        sans: ['var(--font-lufga)', 'sans-serif'],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./app/**/*.{js,ts,jsx,tsx,mdx}",
+      "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+      extend: {
+        // Make sure there is a comma after any property defined *above* this block
+        fontFamily: {
+          sans: ["var(--font-lufga)", "sans-serif"],
+        }, // <- Check closing braces and commas here
       },
     },
-  }
+    plugins: [],
+  };
